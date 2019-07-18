25e59e7f35d49bc6728217e5ed99bc241828fcde
Musa Bello <mbibrahim.4@gmail.com>
2019-07-18 23:16:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -38,9 +38,7 @@ function switchAge(age) {
     case (age >=13 && age <=19):
       return "You are a teenager"
       break;
-    case (age > 19):
-      return "You have an age"
-      break;
     default:
+    return "You have an age"
   }
 }
